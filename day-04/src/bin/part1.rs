fe3743d7a7b35d1bc40a06ad09468d3cd03755aa
parnavh <parnav100sach@gmail.com>
2023-12-06 23:35:56 +0530

    day 04

---
+++ day-04/src/bin/part1.rs
@@ -0,0 +1,59 @@
+use std::collections::HashMap;
+
+fn main() {
+    let input = include_str!("../../input.txt");
+    let output = process(input);
+    dbg!(output);
+}
+
+fn process(input: &str) -> String {
+    let lines = input.lines();
+
+    lines
+        .map(|line| {
+            let mut split = line.split(":").last().unwrap().split("|").map(|s| s.trim());
+
+            let mut win_map: HashMap<&str, bool> = HashMap::new();
+
+            split.next().unwrap().split_whitespace().for_each(|s| {
+                win_map.insert(s, true);
+            });
+
+            let cards = split
+                .next()
+                .unwrap()
+                .split_whitespace()
+                .collect::<Vec<&str>>();
+
+            let count = cards
+                .iter()
+                .map(|card| if win_map.contains_key(card) { 1 } else { 0 })
+                .sum::<u32>();
+
+            if count == 0 {
+                count
+            } else {
+                (2 as u32).pow(count - 1)
+            }
+        })
+        .sum::<u32>()
+        .to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = process(
+            "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+        );
+        assert_eq!(result, "13");
+    }
+}
